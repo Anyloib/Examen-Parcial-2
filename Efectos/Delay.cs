9430ebd8804a0d0b37001f4cc33bf5ce81f02989
Anyloib <anyloib@gmail.com>
2018-03-22 10:41:41 -0700

    Agregar archivos de proyecto.

---
+++ Efectos/Delay.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    class Delay : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        public int offsetTiempoMS;
+        public int offsetRetraso;
+
+        List<float> muestras = new List<float>();
+
+        public Delay(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+            offsetTiempoMS = 600;
+            //50ms - 5000ms
+            offsetRetraso = 500;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+
+        //Offset es el numero de muestras leídas hasta ahorita
+        public int Read(float[] buffer, int offset, int count)
+        {
+            //Calculo de tiempos
+            var read = fuente.Read(buffer, offset, count);
+            float tiempoTranscurrido =
+               (float) muestras.Count / (float)fuente.WaveFormat.SampleRate;
+            int muestrasTranscurridas = muestras.Count;
+            float tiempoTranscurridoMS = tiempoTranscurrido * 1000;
+            int numMuestrasOffsetTiempo = (int)
+                (((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
+
+            //Añadir muestras a nuestro buffer
+            for (int i = 0; i < read; i++)
+            {
+                muestras.Add(buffer[i]);
+            }
+
+
+            //Modificar muestras
+            if ( tiempoTranscurridoMS > offsetTiempoMS)
+            {
+                for (int i = 0; i < read; i++)
+                {
+                    
+                    buffer[i] +=
+                        muestras[muestrasTranscurridas+
+                        i-numMuestrasOffsetTiempo];
+                }
+            }
+
+            else if (tiempoTranscurridoMS < offsetRetraso)
+            {
+                for(int i = 0; i > read; i++)
+                {
+                    buffer[i] *= muestras[muestrasTranscurridas - i + numMuestrasOffsetTiempo];
+                }
+            }
+
+            return read;
+        }
+    }
+}
